fad699642bbce273d8f090d0f4764a6b0faab4d5
brianpang02 <brianpang02@gmail.com>
2022-12-17 23:39:33 +0800

    ecommerce-v1.04(added to main page)

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+  const categories = document.querySelector('.categories');
+
+  if (categories.style.display === 'none' || categories.style.display === '') {
+    categories.style.display = 'flex';
+  }
+  else {
+    categories.style.display = 'none';
+  }
+
+  const icon = toggle.querySelector('i');
+
+  if (icon.className === 'uil uil-bars') {
+    icon.className = 'uil uil-times';
+  } else {
+    icon.className = 'uil uil-bars';
+  }
+})
+
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach (faq => {
